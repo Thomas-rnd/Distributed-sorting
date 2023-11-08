671af3edb96123d5d71b74bdbcbe9a900a9d0202
AlexDevauchelle <devauchelle.alex@gmail.com>
2023-11-08 11:16:07 +0900

    Master threading

--- test_JAVA_implementation/Master.java
+++ test_JAVA_implementation/Master.java
@@ -1,9 +1,6 @@
 import java.io.*;
 import java.net.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -11,8 +8,8 @@ public class Master {
     private static final int port = 9999;
     private static final int maxWorkers = 1; // Définissez le nombre maximal de workers
 
-    private static List<String> registeredIPs = new ArrayList<>();
-    private static Map<String, List<String>> samplingKeys = new HashMap<>();
+    private static Map<String, WorkerInfo> workerInfoMap = new HashMap<>();
+    protected static Map<String, List<String>> samplingKeys = new HashMap<>();
     private static int connectedWorkers = 0;
 
     public static void main(String[] args) {
@@ -21,7 +18,7 @@ public class Master {
             System.out.println("Le maître écoute sur le port " + port);
             ExecutorService threadPool = Executors.newCachedThreadPool();
 
-            while (true) {
+            while (connectedWorkers < maxWorkers) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Nouvelle connexion entrante");
 
@@ -31,13 +28,16 @@ public class Master {
                 if (receivedObject instanceof RegisterRequest) {
                     RegisterRequest registerRequest = (RegisterRequest) receivedObject;
                     handleRegisterRequest(clientSocket, registerRequest, threadPool);
-                } else if (receivedObject instanceof SamplingKeyReply) {
-                    SamplingKeyReply samplingKeyReply = (SamplingKeyReply) receivedObject;
-                    handleSamplingKeyReply(samplingKeyReply);
+                } else {
+                    clientSocket.close();
                 }
 
-                clientSocket.close();
             }
+            serverSocket.close();
+
+            // send samplingkeyrequest
+            sendSamplingKeyRequests();
+
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
@@ -48,31 +48,37 @@ public class Master {
         if (connectedWorkers < maxWorkers) {
             String clientIP = clientSocket.getInetAddress().getHostAddress();
             System.out.println("IP enregistrée : " + clientIP);
-            registeredIPs.add(clientIP);
+
+            // Crée un WorkerInfo avec le clientIP et la socket associée
+            WorkerInfo workerInfo = new WorkerInfo(clientIP, clientSocket);
+            workerInfoMap.put(clientIP, workerInfo);
+
             connectedWorkers++;
             RegisterReply reply = new RegisterReply();
-            Socket workerSocket = new Socket(workerIP, workerPort);
-            sendObject(workerSocket, reply);
-            workerSocket.close();
-            if (connectedWorkers == maxWorkers) {
-                sendSamplingKeyRequests();
-            }
+
+            // Utilise la socket du client pour envoyer la RegisterReply
+            sendObject(clientSocket, reply);
+
         }
     }
 
     private static void sendSamplingKeyRequests() {
-        for (String workerIP : registeredIPs) {
-            Thread thread = new Thread(new SamplingKeyRequestThread(workerIP));
+        List<Thread> threads = new ArrayList<>();
+
+        for (WorkerInfo workerInfo : workerInfoMap.values()) {
+            Thread thread = new Thread(new SamplingKeyRequestThread(workerInfo));
+            threads.add(thread);
             thread.start();
         }
-    }
 
-    private static void handleSamplingKeyReply(SamplingKeyReply samplingKeyReply) {
-        String workerIP = samplingKeyReply.getWorkerIP();
-        List<String> keys = samplingKeyReply.getSamplingKeys();
-
-        System.out.println("Keys from worker " + workerIP + ": " + keys);
-        samplingKeys.put(workerIP, keys);
+        // Attend que tous les threads aient terminé
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private static void sendObject(Socket socket, Serializable obj) {
@@ -86,23 +92,58 @@ public class Master {
 }
 
 class SamplingKeyRequestThread implements Runnable {
-    private String workerIP;
-    private int workerPort = 9998;
+    private WorkerInfo workerInfo;
 
-    public SamplingKeyRequestThread(String workerIP) {
-        this.workerIP = workerIP;
+    public SamplingKeyRequestThread(WorkerInfo workerInfo) {
+        this.workerInfo = workerInfo;
     }
 
     @Override
     public void run() {
         try {
-            Socket socket = new Socket(workerIP, workerPort);
+            Socket socket = workerInfo.getSocket();
             ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+
+            // Envoyer la requête SamplingKeyRequest
             SamplingKeyRequest request = new SamplingKeyRequest();
             out.writeObject(request);
-            socket.close();
-        } catch (IOException e) {
+
+            // Attendre la réponse SamplingKeyReply
+            Object receivedObject = in.readObject();
+            if (receivedObject instanceof SamplingKeyReply) {
+                SamplingKeyReply reply = (SamplingKeyReply) receivedObject;
+                handleSamplingKeyReply(reply);
+            }
+
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    private void handleSamplingKeyReply(SamplingKeyReply samplingKeyReply) {
+        String workerIP = workerInfo.getWorkerIP();
+        List<String> keys = samplingKeyReply.getSamplingKeys();
+
+        System.out.println("Keys from worker " + workerIP + ": " + keys);
+        samplingKeys.put(workerIP, keys);
+    }
+}
+
+class WorkerInfo {
+    private String workerIP;
+    private Socket socket;
+
+    public WorkerInfo(String workerIP, Socket socket) {
+        this.workerIP = workerIP;
+        this.socket = socket;
+    }
+
+    public String getWorkerIP() {
+        return workerIP;
+    }
+
+    public Socket getSocket() {
+        return socket;
     }
 }
