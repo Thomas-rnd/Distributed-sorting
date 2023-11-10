4c5d036bde73cbac6b4970ee3f0b669c80b8e23b
AlexDevauchelle <devauchelle.alex@gmail.com>
2023-11-11 00:46:55 +0900

    Remove Test Project

--- test_JAVA_implementation/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Vérifie si le répertoire de compilation existe, s'il n'existe pas, le crée
-if [ ! -d "bin" ]; then
-  mkdir bin
-fi
-
-# Compile tous les fichiers Java dans le répertoire courant et les place dans le répertoire "bin"
-find . -name "*.java" | xargs javac -d bin
