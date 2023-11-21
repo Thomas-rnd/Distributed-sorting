328dc3b41622d24c1198004ed17a585417aa7888
Thomas RENAUD <renaudthomas@orange.fr>
2023-11-21 14:09:16 +0900

    key refactoring

--- sortnet/core/src/main/scala/Key.scala
+++ sortnet/core/src/main/scala/Key.scala
@@ -1,12 +1,21 @@
 package com.cs434.sortnet.core
+import java.io.IOException
 
-import java.nio.ByteBuffer
-import java.io.{DataInputStream, DataOutputStream, IOException}
-import java.util.Arrays
-
+/**
+ * Represents a key used in sorting and partitioning.
+ *
+ * @param bytes The byte array representation of the key.
+ */
 @SerialVersionUID(3403353340572833574L)
 case class Key(bytes: Array[Byte]) extends Ordered[Key] with Serializable {
-  // Implement the compare method required by Ordered
+
+  /**
+   * Compares this key with another key.
+   *
+   * @param that The other key to compare with.
+   * @return A negative integer, zero, or a positive integer as this key is less than, equal to,
+   *         or greater than the specified key.
+   */
   def compare(that: Key): Int = {
     val thisBytes = this.bytes
     val thatBytes = that.bytes
@@ -15,6 +24,7 @@ case class Key(bytes: Array[Byte]) extends Ordered[Key] with Serializable {
     var i = 0
     while (i < length) {
       val cmp = thisBytes(i) - thatBytes(i)
+      assert(cmp >= Byte.MinValue && cmp <= Byte.MaxValue, "Comparison result out of byte range")
       if (cmp != 0) return cmp
       i += 1
     }
@@ -23,11 +33,16 @@ case class Key(bytes: Array[Byte]) extends Ordered[Key] with Serializable {
     thisBytes.length - thatBytes.length
   }
 
-  // Serialize the Key to a byte array
+  /**
+   * Serializes the Key to a byte array.
+   *
+   * @return The serialized byte array.
+   */
   def toByteArray: Array[Byte] = {
     try {
       val byteArray = new Array[Byte](bytes.length)
-      System.arraycopy(bytes, 0, byteArray, 0, bytes.length)
+      Array.copy(bytes, 0, byteArray, 0, bytes.length)
+      assert(java.util.Arrays.equals(byteArray, bytes), "Array copy failed")
       byteArray
     } catch {
       case e: IOException =>
@@ -35,13 +50,21 @@ case class Key(bytes: Array[Byte]) extends Ordered[Key] with Serializable {
     }
   }
 
+  /**
+   * Returns a string representation of the key.
+   *
+   * @return The string representation.
+   */
   override def toString: String = {
     val byteArrayAsString = new String(bytes, "UTF-8")
     s"Key([$byteArrayAsString])"
   }
-  
 
-  // Function to increment the key value by 1
+  /**
+   * Increments the key value by 1.
+   *
+   * @return The new Key after incrementing.
+   */
   def incrementByOne: Key = {
     val incrementedBytes = new Array[Byte](bytes.length)
     var carry = 1
@@ -53,19 +76,24 @@ case class Key(bytes: Array[Byte]) extends Ordered[Key] with Serializable {
       carry = sum >> 8
     }
 
-    // If there's still a carry after the loop, it means we've overflowed, we assume that case never happens
+    // If there's still a carry after the loop, it means we've overflowed
     if (carry != 0) {
       // Handle overflow by creating a new larger byte array and copying the incrementedBytes into it
       val newBytes = new Array[Byte](bytes.length + 1)
-      System.arraycopy(incrementedBytes, 0, newBytes, 1, bytes.length)
+      Array.copy(incrementedBytes, 0, newBytes, 1, bytes.length)
       newBytes(0) = 1 // Set the most significant byte to 1
+      assert(newBytes.sameElements(incrementedBytes ++ Array[Byte](1)), "Increment overflow failed")
       Key(newBytes)
     } else {
       Key(incrementedBytes)
     }
   }
 
-  // Function to represent the bytes as integers in the range [0, 255]
+  /**
+   * Represents the bytes as integers in a string.
+   *
+   * @return The string representation of bytes as integers.
+   */
   def toStringAsIntArray: String = {
     val intArray = bytes.map(byteValue => byteValue & 0xFF)
     s"Key[${intArray.mkString(" ")}]"
@@ -76,6 +104,11 @@ case class Key(bytes: Array[Byte]) extends Ordered[Key] with Serializable {
 object Key {
   val keySize = 10 // The size of the key in bytes
 
-  // Deserialize a Key from a byte array
+  /**
+   * Deserializes a Key from a byte array.
+   *
+   * @param byteArray The byte array to deserialize.
+   * @return A new Key instance.
+   */
   def fromByteArray(byteArray: Array[Byte]): Key = Key(byteArray)
 }
